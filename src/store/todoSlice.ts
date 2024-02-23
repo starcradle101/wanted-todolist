7691a217c53bcf7778a662b6e141603670bbf48c
Hoonie <starcradle@Hoonieui-MacBookPro.local>
2024-02-23 14:33:07 +0900

    Refactor: 불필요한 코드 삭제
    todoSlice.ts에서 불필요한 코드 삭제

--- src/store/todoSlice.ts
+++ src/store/todoSlice.ts
@@ -1,5 +1,4 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import type { RootState } from './store';
 
 interface TodoState {
 	list: Array<string>;
@@ -23,5 +22,4 @@ export const todoSlice = createSlice({
 });
 
 export const { addition, deletion } = todoSlice.actions;
-export const selectCount = (state: RootState) => state.todo.list;
 export default todoSlice.reducer;
